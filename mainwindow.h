d05053ae391de9ff123caf88930cd1dfb18746cb
Emil Ekmečić <e.ekmecic@gmail.com>
2016-11-04 15:05:55 -0400

    First proof of concept
    Super sketchy and messy code.
    This revision displays some randomly generated data in the dataTable
    that's generated every time a QTimer fires.

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,8 @@
-#ifndef MAINWINDOW_H
+#pragma once
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_manualThrottleButton_clicked();
+
+    void on_ESTOPButton_clicked();
+
+    void on_scmSPButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
-
-#endif // MAINWINDOW_H
